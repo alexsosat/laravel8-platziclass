0410580531397371731e660d4555812f0b480744
alexsosat <56005779+alexsosat@users.noreply.github.com>
2021-04-30 07:39:53 -0500

    Components in laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,4 +6,6 @@
         <h2 class="text-xl text-gray-600">Fórmate online como profesional de tecnología</h2>
         <h3 class="text-lg text-gray-600">70% de los graduandos duplica sus ingresos</h3>
     </div>
-@endsection
+
+    <livewire:course-list>
+    @endsection
